1bf76149b6f812ff52e8be65afbefa67ba3d6c2b
Czerwcuu - komputer <czerwiecr1999@gmail.com>
2019-11-21 19:25:32 +0100

    0.0|created project, create paddles,ball with movement and ball colision with borders

---
+++ game.py
@@ -0,0 +1,86 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pingpong by Stylesowy")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# g1
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# g2
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
+# Function
+def g1_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def g1_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def g2_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def g2_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# keyboard bindsing
+wn.listen()
+wn.onkeypress(g1_up, "w")
+wn.onkeypress(g1_down, "s")
+wn.onkeypress(g2_up, "Up")
+wn.onkeypress(g2_down, "Down")
+
+# pilka
+ball = turtle.Turtle()
+ball.speed(0)
+ball.color("green")
+ball.shape("circle")
+ball.goto(0, 0)
+ball.dx = 0.3
+ball.dy = -0.3
+# Main loop
+while True:
+    wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor()+ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border check
+    if ball.ycor()>290:
+        ball.sety(290)
+        ball.dy *= -1
+    if ball.ycor()<-290:
+        ball.sety(-290)
+        ball.dy *= -1
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
