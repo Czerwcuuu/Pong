fcf6b1b3439eb0a7bd1101eed2e412659b0cadc5
Czerwcuu - komputer <czerwiecr1999@gmail.com>
2019-11-22 00:08:21 +0100

    0.2|count score

--- game.py
+++ game.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # g1
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,10 +41,12 @@ def g1_down():
     y -= 20
     paddle_a.sety(y)
 
+
 def g2_up():
     y = paddle_b.ycor()
     y += 20
     paddle_b.sety(y)
+
 
 def g2_down():
     y = paddle_b.ycor()
@@ -63,27 +69,43 @@ ball.shape("circle")
 ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 # Main loop
 while True:
     wn.update()
 
     # Move the ball
-    ball.setx(ball.xcor()+ball.dx)
+    ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
     # Border check
-    if ball.ycor()>290:
+    if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-    if ball.ycor()<-290:
+    if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
     if ball.xcor() > 390:
-        ball.goto(0,0)
+        ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
     if ball.xcor() < -390:
-        ball.goto(0,0)
+        ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
     # paddle and ball collisions
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
